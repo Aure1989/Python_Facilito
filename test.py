812e43d0a2eb5752254e2da00efd90a8e283be32
KuroDev <aurelio.dc.89@gmail.com>
2020-06-26 22:56:54 -0500

    python 3 course

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
-dicc = {'a':55, 22:"Esto es texto o string", 'a':125}
-dicc['c']="Nicole no te ama"
+my_function=lambda x,y: x+y
 
-del dicc['c']
-
-value= dicc.get('z', "No existe esa clave")
-
-llaves= list(dicc.keys())
-print(llaves)
-print(dicc)
+result=my_function(10,20)
+print(result)
