8161d2a5db133dee49e0bbbc6e45a2a9e8ac9a97
KuroDev <aurelio.dc.89@gmail.com>
2020-07-03 21:08:52 -0500

    Llegamos hasta la clase de decoradores

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-my_function=lambda x,y: x+y
+def decorator(func):
+    def new_func(*args,**kwargs):
+        print("Ejecutar función")
+        resultado=func(*args,**kwargs) 
+        print("Función ejecutada")
+        return resultado
+    return new_func
 
-result=my_function(10,20)
-print(result)
+
+@decorator
+def suma(n1,n2):
+    return n1+n2
+
+resultado2=suma(25,5)
+print(resultado2)
